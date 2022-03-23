1163c198597b349f597856f668fececa02e3d727
Salwa Ul Haq <salwa.suh@gmail.com>
2022-03-23 15:29:05 +0700

    fetch data to jsx

--- src/App.js
+++ src/App.js
@@ -1,12 +1,18 @@
-// import logo from './logo.svg';
 import './App.css';
+import data from './data.js';
 // import Search from './search';
 
 function App() {
   // const clientId = process.env.REACT_APP_CLIENT_KEY;
   return (
-    <div className="App">
-      
+    <div className="container">
+      <h1>Playlist Music</h1>
+      <div className="cardPlaylist">
+        <img src={data.album.images[0].url} alt="imagePlaylist"/>
+        <h3>{data.album.name}</h3>
+        <p>{data.album.artists[0].name}</p>
+        <button className="btnSelect">Select</button>
+      </div>
       {/* <Search /> */}
     </div>
   );
