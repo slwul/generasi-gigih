0d89078b5bfba2bed98539934c42c334a456313a
Salwa Ul Haq <salwa.suh@gmail.com>
2022-03-23 15:56:33 +0700

    customize layout and css for playlist

--- src/App.js
+++ src/App.js
@@ -6,10 +6,10 @@ function App() {
   // const clientId = process.env.REACT_APP_CLIENT_KEY;
   return (
     <div className="container">
-      <h1>Playlist Music</h1>
+      <h1 className="titlePlaylist">Playlist Music</h1>
       <div className="cardPlaylist">
         <img src={data.album.images[0].url} alt="imagePlaylist"/>
-        <h3>{data.album.name}</h3>
+        <h3 className="albumName">{data.album.name}</h3>
         <p>{data.album.artists[0].name}</p>
         <button className="btnSelect">Select</button>
       </div>
