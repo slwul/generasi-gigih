b1a0010a93178b479230da1fe98db24936dc83ff
Salwa Ul Haq <salwa.suh@gmail.com>
2022-03-23 14:53:33 +0700

    adding data spotify to data.js

--- src/App.js
+++ src/App.js
@@ -1,26 +1,13 @@
-import logo from './logo.svg';
+// import logo from './logo.svg';
 import './App.css';
-import Search from './search';
+// import Search from './search';
 
 function App() {
-  const clientId = process.env.REACT_APP_CLIENT_KEY;
+  // const clientId = process.env.REACT_APP_CLIENT_KEY;
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-        <Search />
-      </header>
+      
+      {/* <Search /> */}
     </div>
   );
 }
