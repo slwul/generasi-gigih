4050977a4f6fc7fee9f87500e5040f7939d9eb50
Salwa Ul Haq <salwa.suh@gmail.com>
2022-03-25 11:49:54 +0700

    convert track elements into components

---
+++ src/components/Track/index.js
@@ -0,0 +1,12 @@
+const Track = ({ url, title, artist }) => {
+    return (
+        <div className="cardPlaylist">
+            <img src={url} alt="imagePlaylist"/>
+            <h3 className="albumName">{title}</h3>
+            <p>{artist}</p>
+            <button className="btnSelect">Select</button>
+        </div>
+    );
+};
+    
+export default Track;
