d17cd1d7c3c8ef152d80fb15ea96d64bfcb3e224
Salwa Ul Haq <salwa.suh@gmail.com>
2022-03-30 16:58:01 +0700

    mod3-ses1 authorized to spotify & add search

--- src/App.js
+++ src/App.js
@@ -1,23 +1,10 @@
 import "./App.css";
-import data from "./data.js";
-import Track from "./components/Track";
+import Home from "./pages/Home";
 
 function App() {
   // const clientId = process.env.REACT_APP_CLIENT_KEY;
-  const trackList = data.map( (track) => (
-    <Track
-        key={track.id}
-        url={track.album.images[0].url}
-        title={track.album.name}
-        artist={track.album.artists[0].name}
-      />
-  ));
-  return (
-    <div className="container">
-      <h1 className="titlePlaylist">Playlist Music</h1>
-      <div className="trackList">{trackList}</div>
-    </div>
-  );
+  return 
+    <Home />;
 }
 
 export default App;
