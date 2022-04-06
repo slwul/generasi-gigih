581acc93d7acc0d31655de3ccb28740c0c5759b0
Salwa Ul Haq <salwa.suh@gmail.com>
2022-04-06 07:38:57 +0700

    created button select and deselect

--- src/components/Track/index.js
+++ src/components/Track/index.js
@@ -1,12 +1,24 @@
-const Track = ({ url, title, artist }) => {
+import React, { useState} from "react";
+
+export default function Track({ url, title, artist, toggleSelect }) {
+    const [isSelected, setIsSelected] = useState(false);
+
+    const handleSelect = () => {
+        setIsSelected(!isSelected);
+        toggleSelect();
+    };
     return (
         <div className="cardPlaylist">
             <img src={url} alt="imagePlaylist"/>
             <h3 className="albumName">{title}</h3>
             <p className="artistName">{artist}</p>
-            <button className="btn-primary btnSelect">Select</button>
+            <button 
+                className={`btn btnSelect ${
+                isSelected ? "btnPrimary" : "btnSecondary"}`}
+                onClick={handleSelect}>
+                {isSelected ? "Deselect" : "Select"}
+            </button>
         </div>
     );
-};
+}
     
-export default Track;
